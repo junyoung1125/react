38f327e136e3876ae2da5cb5dbe2dbc715790fbd
junyoung1125 <wnsdud060451@gmail.com>
2025-05-28 09:11:33 +0900

    commit

--- farmstory-front/src/routers/router.jsx
+++ farmstory-front/src/routers/router.jsx
@@ -1,27 +1,78 @@
-import { createBrowserRouter} from 'react-router-dom';
+import { createBrowserRouter } from "react-router-dom";
+import { MainPage } from "../pages/main/MainPage";
+import { HelloPage } from "../pages/introduction/HelloPage";
+import { DirectionPage } from "../pages/introduction/DirectionPage";
+import { ListPage } from "../pages/market/ListPage";
+import { LoginPage } from "../pages/user/LoginPage";
+import { TermsPage } from "../pages/user/TermsPage";
+import { RegisterPage } from "../pages/user/RegisterPage";
+import { AdminMainPage } from "../pages/admin/AdminMainPage";
+import { lazy, Suspense } from "react";
+
+// prettier-ignore
+const AdminProductRegisterPage = lazy(() =>  import("../pages/admin/product/RegisterPage"));
+const BoardListPage = lazy(() => import("../pages/board/ListPage"));
+const BoardWritePage = lazy(() => import("../pages/board/WritePage"));
+const BoardViewPage = lazy(() => import("../pages/board/ViewPage"));
+const BoardModifyPage = lazy(() => import("../pages/board/ModifyPage"));
 
 // 라우터 생성
-// 위에 import 수동생성
 const router = createBrowserRouter([
-    {path:"/", element: null},
-    {path:"/introduction/hello", element: null},
-    {path:"/introduction/direction", element: null},
-    {path:"/market/list", element: null},
-    {path:"/croptalk/story", element: null},
-    {path:"/croptalk/grow", element: null},
-    {path:"/croptalk/school", element: null},
-    {path:"event/info", element: null},
-    {path:"/community/notice", element: null},
-    {path:"/user/login", element: null},
-    {path:"/user/terms", element: null},
-    {path:"/user/logout", element: null},
-    {path:"/board/list", element: null},
-    {path:"/board/write", element: null},
-    {path:"/board/view", element: null},
-    {path:"/board/modify", element: null},
-    {path:"/admin", element: null},
-    {path:"/admin/product/register", element: null}
+  { path: "/", element: <MainPage /> },
+
+  { path: "/introduction/hello", element: <HelloPage /> },
+  { path: "/introduction/direction", element: <DirectionPage /> },
+
+  { path: "/market/list", element: <ListPage /> },
+
+  { path: "/user/login", element: <LoginPage /> },
+  { path: "/user/terms", element: <TermsPage /> },
+  { path: "/user/register", element: <RegisterPage /> },
+  { path: "/user/logout", element: null },
+
+  {
+    path: "/board/list",
+    element: (
+      <Suspense>
+        <BoardListPage />
+      </Suspense>
+    ),
+  },
+  {
+    path: "/board/write",
+    element: (
+      <Suspense>
+        <BoardWritePage />
+      </Suspense>
+    ),
+  },
+  {
+    path: "/board/view",
+    element: (
+      <Suspense>
+        <BoardViewPage />
+      </Suspense>
+    ),
+  },
+  {
+    path: "/board/modify",
+    element: (
+      <Suspense>
+        <BoardModifyPage />
+      </Suspense>
+    ),
+  },
+
+  { path: "/admin", element: <AdminMainPage /> },
+  {
+    path: "/admin/product/register",
+    element: (
+      <Suspense>
+        <AdminProductRegisterPage />
+      </Suspense>
+    ),
+  },
 ]);
 
-//라우터 내보내기
+// 라우터 내보내기
 export default router;
